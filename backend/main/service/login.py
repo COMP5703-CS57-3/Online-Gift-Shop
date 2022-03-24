38f97d5a5e835d098a39e5e74445ba6e35c7189a
ZhengLi74 <101176546+ZhengLi74@users.noreply.github.com>
2022-03-24 19:03:21 +0800

    Add files via upload

--- backend/main/service/login.py
+++ backend/main/service/login.py
@@ -7,34 +7,47 @@ def login_method(user_input_dictionary):
     output_message = {
         "message": "Information waiting for confirmation"
     }
-    print(user_input_dictionary)
+    output_message_200 = {
+        "user_name": "",
+        "message": "Information waiting for confirmation"
+    }
     # this is a login function, so need to check the user row in User table by user email
     user = User.query.filter_by(user_email=user_input_dictionary["user_email"]).first()
-    # this situation is the user input incorrect password
+    # this situation is the user did not exit
     if user is None:
         status_code = 403
-        output_message['user_message'] = "User did not exit, please sign up first"
+        output_message['message'] = "User did not exit, please sign up first"
+    # this situation is the user input incorrect password
     elif user.user_password != user_input_dictionary["user_password"]:
         status_code = 400
         output_message['message'] = 'Please input correct password'
-    # this situation is the user did not exit
     # this situation is the user's password is correct
     elif user.user_password == user_input_dictionary["user_password"]:
         status_code = 200
-        output_message['user_message'] = "User login successfully"
+        output_message_200['user_name'] = user.user_name
+        output_message_200['message'] = "User login successfully"
     else:
         status_code = 404
-        output_message['user_message'] = "Unknown error"
-    # use make_response function add output_message's infor and dictionary format to output_json
-    output_json = make_response(output_message)
-    # the status_code should be 200, 400, 403, 404
-    output_json.status_code = status_code
-    # pass the 'message' information in above 4 types
-    output_json.message = output_message['message']
-    # if user exits, will pass the user's id to front-end
+        output_message['message'] = "Unknown error"
     if user:
+        # use make_response function add output_message's infor and dictionary format to output_json
+        output_json = make_response(output_message_200)
+        # the status_code should be 200, 400, 403, 404
+        output_json.status_code = status_code
+        # pass the 'message' information in above 4 types
         output_json.id = user.id
-    # if user did not exit, will pass 'id = 0' to front-end
+        output_json.message = output_message_200['message']
+        output_json.user_name = output_message_200['user_name']
+        # if user exits, will pass the user's id to front-end
+
     else:
+        # use make_response function add output_message's infor and dictionary format to output_json
+        output_json = make_response(output_message)
+        # the status_code should be 200, 400, 403, 404
+        output_json.status_code = status_code
+        # pass the 'message' information in above 4 types
+        output_json.message = output_message['message']
+        # if user exits, will pass the user's id to front-end
         output_json.id = 0
+
     return output_json
