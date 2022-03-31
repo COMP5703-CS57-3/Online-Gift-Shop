e0e50eb44afd95a37bb798aab2b7cfff0ce791e3
ZhengLi74 <101176546+ZhengLi74@users.noreply.github.com>
2022-03-31 19:26:59 +0800

    Add files via upload

--- backend/main/connect_to_aws.py
+++ backend/main/connect_to_aws.py
@@ -2,8 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 # connect our team's aws cloud server
 database = SQLAlchemy()
-aws_endpoint = "mysql+pymysql://ZhengLi:Comp5703;@database-comp5703-cs57-3.cvbkv8f5esm6.us-east-1.rds.amazonaws.com" \
-               ":3306/comp5703"
+aws_endpoint = "mysql+pymysql://ZhengLi:Comp5703;@database-comp5703-cs57-3.cvbkv8f5esm6.us-east-1.rds.amazonaws.com:3306/comp5703"
 
 
 
