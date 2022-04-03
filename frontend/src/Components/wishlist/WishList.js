569929b64dae2514d2d9002501fa4893a49d8423
Shuyuan Liu <sliu6587@uni.sydney.edu.au>
2022-04-03 22:09:24 +0800

    1
    1

--- frontend/src/Components/wishlist/WishList.js
+++ frontend/src/Components/wishlist/WishList.js
@@ -1,25 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import CartProvider from "../../tools/useCart";
 
 import WishProvider from "../../tools/useWish";
 import CategoryW from "./CategoryW";
 import Button from "@mui/material/Button";
 import owner from "../../data/owner.json"
+import Wishlist from "../../data/Wishlist2.json"
 
 export default function WishList() {
-    const formData = new FormData();
-    formData.append("owner_id",1);
-    fetch("http://127.0.0.1:5000/wishlist/show",
-        {
+    const [data,setData] = useState(Wishlist);
+    fetch("http://127.0.0.1:5000/wishlist/show", {
             method: 'POST',
-            body: {owner_id:1},
-            headers: {
-            'Content-Type': 'application/json'
-          }
-        }).then(console.log)
+            body: JSON.stringify({owner_id:1})
+        }).then(res=>res.json()).then(setData);
+    console.log(data)
     return (
         <WishProvider>
-            <Button>show</Button>
             <CategoryW/>
         </WishProvider>
     )
