e8e7f7a9f33d932577859ccb541f61b9559d8976
wtxqooi <2910842215@qq.com>
2022-04-06 10:12:37 +0800

    login redirection

--- frontend/src/Components/login.js
+++ frontend/src/Components/login.js
@@ -1,11 +1,12 @@
 import * as React from 'react';
+import {useState} from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import { useNavigate } from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
@@ -13,46 +14,40 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import {createTheme, ThemeProvider} from '@mui/material/styles';
 import axios from "axios";
-import {Link} from "react-router-dom";
 import Copyright from "./cpright";
 import FastDial from "./FastDial";
 import {checkEmail} from "../logic/ValCheck";
 
 const theme = createTheme();
 
-export default class LogIn extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            Email: "",
-            Password: "",
-        }
-    }
+export default function LogIn(props) {
 
-    HandleChange = (e) => {
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+    const HandleChange = (e) => {
         const name = e.target.name
         const value = e.target.value
         // console.log(name,value)
         // console.log(this)
         this.setState({[name]: value},)
     }
-    HandleClick = () => {
-        const navi= useNavigate()
-        if (this.state.Password === "") {
+    const navigate=useNavigate()
+    const HandleClick = () => {
+        if (Password === "") {
             alert("please input password")
-        } else if (checkEmail(this.state.Email) !== true) {
+        } else if (checkEmail(Email) !== true) {
             alert("please input valid Email")
         } else {
             // console.log(this.state.Username,this.state.Password)
             axios.post('http://localhost:5000/login_signup/login', {
-                user_email: this.state.Email,
-                user_password: this.state.Password
+                user_email: Email,
+                user_password: Password
             }).then((response) => {
                 let status = response;
                 console.log(status, 1)
                 if (status.data.message === 'User login successfully') {
                     console.log("Success!")
-                    navi("/")
+                    navigate("/")
                 } else {
                     console.log("Error!")
                 }
@@ -70,83 +65,82 @@ export default class LogIn extends React.Component {
     }
 
 
-    render() {
-        return (
-            <ThemeProvider theme={theme}>
+    return (
+        <ThemeProvider theme={theme}>
 
-                <Container component="main" maxWidth="xs">
-                    <CssBaseline/>
-                    <Box
-                        sx={{
-                            marginTop: 8,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                        }}
-                    >
-                        <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
-                            <LockOutlinedIcon/>
-                        </Avatar>
-                        <Typography component="h1" variant="h5">
+            <Container component="main" maxWidth="xs">
+                <CssBaseline/>
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
+                        <LockOutlinedIcon/>
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign In
+                    </Typography>
+
+                    <Box component="form" noValidate sx={{mt: 1}}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="Email"
+                            autoComplete="email"
+                            autoFocus
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="Password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary"/>}
+                            label="Remember me"
+                        />
+                        <Button
+                            // type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{mt: 3, mb: 2}}
+                            onClick={() => HandleClick()}
+                        >
                             Sign In
-                        </Typography>
+                        </Button>
 
-                        <Box component="form" noValidate sx={{mt: 1}}>
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="email"
-                                label="Email Address"
-                                name="Email"
-                                autoComplete="email"
-                                autoFocus
-                                onChange={(e) => this.HandleChange(e)}
-                            />
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                name="Password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                autoComplete="current-password"
-                                onChange={(e) => this.HandleChange(e)}
-                            />
-                            <FormControlLabel
-                                control={<Checkbox value="remember" color="primary"/>}
-                                label="Remember me"
-                            />
-                            <Button
-                                // type="submit"
-                                fullWidth
-                                variant="contained"
-                                sx={{mt: 3, mb: 2}}
-                                onClick={() => this.HandleClick()}
-                            >
-                                Sign In
-                            </Button>
-
-                            <Grid container>
-                                <Grid item xs>
-                                    <Link to={{pathname: "/findpwd"}}>
-                                        Forgot password?
-                                    </Link>
-                                </Grid>
-                                <Grid item>
-                                    <Link to={{pathname: "/signup"}}>
-                                        Sign Up
-                                    </Link>
-                                </Grid>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link to={{pathname: "/findpwd"}}>
+                                    Forgot password?
+                                </Link>
                             </Grid>
-                        </Box>
-
+                            <Grid item>
+                                <Link to={{pathname: "/signup"}}>
+                                    Sign Up
+                                </Link>
+                            </Grid>
+                        </Grid>
                     </Box>
-                    <Copyright sx={{mt: 8, mb: 4}}/>
-                </Container><FastDial/>
-            </ThemeProvider>
 
-        );
-    }
+                </Box>
+                <Copyright sx={{mt: 8, mb: 4}}/>
+            </Container><FastDial/>
+        </ThemeProvider>
+
+    );
+
 }
