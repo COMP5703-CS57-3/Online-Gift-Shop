a503d83e5f36222ab976e2658589a99f28a1d4d1
wtxqooi <2910842215@qq.com>
2022-04-07 23:18:43 +0800

    route for InviteCode func

--- frontend/src/Components/FastDailContent/InviteCode.js
+++ frontend/src/Components/FastDailContent/InviteCode.js
@@ -1,68 +1,63 @@
 import * as React from 'react';
+import {useState} from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import {ClickAwayListener} from "@mui/material";
 import TextField from "@mui/material/TextField";
+import {useNavigate} from "react-router-dom";
 
 const style = {
     position: 'fixed',
-        top: '50%',
-        left: '50%',
-        width: 400,
-        transform: 'translate(-50%, -70%)',
-        background: '#fff',
-        zIndex:999
+    top: '50%',
+    left: '50%',
+    width: 400,
+    transform: 'translate(-50%, -70%)',
+    background: '#fff',
+    zIndex: 999
 }
-export default class InviteCode extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            InviteCode: "",
-
-        }
-    }
-
+export default function InviteCode() {
+    const [InviteCode, setInviteCode] = useState("")
+    const navigate = useNavigate()
     // HandleClickAway() {
     //     this.setState({visible:true})
+    function HandleClick() {
+        navigate(`/wishlist/${InviteCode}`)
+    }
+
     // }
 
-    render() {
-        return (
-                <Card  sx={{...style,maxWidth: 345,background:'#fff'}} variant="outlined" >
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Invite Code
-                        </Typography>
-                        <TextField
-                            // margin="normal"
-                            required
-                            fullWidth
-                            name="InviteCode"
-                            onChange={(e) => this.HandleChange(e)}
-                        />
-                    </CardContent>
-                    <CardActions>
-                        <Button size="small" onClick={() => this.HandleClick()}>Go!</Button>
-                    </CardActions>
-                </Card>
-        );
-    }
 
-    HandleClick() {
-        console.log(this.state.InviteCode)
-    }
-
-    HandleChange = (e) => {
+    const HandleChange = (e) => {
         const name = e.target.name
         const value = e.target.value
         // console.log(name,value)
         // console.log(this)
-        this.setState({[name]: value},)
+        setInviteCode(value)
 
 
     }
+
+    return (
+        <Card sx={{...style, maxWidth: 345, background: '#fff'}} variant="outlined">
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    Invite Code
+                </Typography>
+                <TextField
+                    // margin="normal"
+                    required
+                    fullWidth
+                    name="InviteCode"
+                    onChange={(e) => HandleChange(e)}
+                />
+            </CardContent>
+            <CardActions>
+                <Button size="small" onClick={() => HandleClick()}>Go!</Button>
+            </CardActions>
+        </Card>
+    );
+
 
 }
