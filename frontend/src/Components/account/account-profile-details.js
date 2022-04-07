da51884b1889260b46fc530552879eeee55d91b1
wtxqooi <2910842215@qq.com>
2022-04-07 18:09:45 +0800

    account

--- frontend/src/Components/account/account-profile-details.js
+++ frontend/src/Components/account/account-profile-details.js
@@ -17,6 +17,7 @@ import Stack from "@mui/material/Stack";
 import {AccountCircle} from "@mui/icons-material";
 import {DatePicker, LocalizationProvider} from "@mui/lab";
 import AdapterDateFns from '@material-ui/lab/AdapterDateFns';
+
 const states = [
     {
         value: 'alabama',
@@ -33,7 +34,7 @@ const states = [
 ];
 const sample = {
     "id": 4,
-    "user_name": "sdfasdfdsaf",
+    "user_name": "local",
     "user_email": "2910842215@qq.com",
     "user_date_of_birth": null,
     "user_mobile": "15636128575",
@@ -66,24 +67,61 @@ export default class AccountProfileDetails extends React.Component {
 
     UNSAFE_componentWillMount() {
         const that = this
-        let id = this.state.id === null ? sessionStorage.getItem("id") : id
-        console.log(id)
+        let id = this.props.id === null ? sessionStorage.getItem("id") : id
         axios.get(`http://localhost:5000/user_information/user_profile/${id}`)
             .then(r => {
                 // console.log(r.data)
-                that.setState({"user": r.data, "isLoad": true})
-                that.setState({"new_user": {"id": r.data.id, "user_email": r.data.user_email}})
-                const address = r.data
+                const address = r.data.user_address ? r.data.user_address.split(",") : ['', '', '']
+                const user = Object.assign(r.data, {
+                    "user_country": address[0] === "" ? "Australia" : address[0],
+                    "user_state": address[1] === "" ? "New South Wales" : address[1],
+                    "user_detail_street": address[2]
+                })
+
+
+                that.setState({
+                    "user": user
+                })
+                that.setState({
+                    "new_user": user
+                })
+                that.setState({"isLoad": true})
+
             })
-            .catch(r => console.log(r))
+            .catch(r => {
+                const user = sample
+                const address = user.user_address ? user.user_address.split(",") : ['', '', '']
+                console.log("运行到这了")
+                that.setState({
+                    "user": Object.assign(user, {
+                        "user_country": address[0] === "" ? "Australia" : address[0],
+                        "user_state": address[1] === "" ? "New South Wales" : address[1],
+                        "user_detail_street": address[2]
+                    })
+                })
+                that.setState({
+                    "new_user": this.state.user
+                })
+                that.setState({"isLoad": true})
+
+            })
     }
 
     handleChange = (event) => {
-        this.setState({
-            new_user: {
-                [event.target.name]: event.target.value
-            }
-        });
+        console.log(this.state)
+        const data = this.state.new_user
+        if (event && event.target) {
+            data[[event.target.name]] = event.target.value
+
+            this.setState({
+                new_user: data
+            });
+        } else {
+            data["user_date_of_birth"] = event.toString().slice(0, -27)
+            this.setState({new_user: data})
+
+        }
+        console.log(this.state.user.user_date_of_birth)
 
     };
 
@@ -114,7 +152,7 @@ export default class AccountProfileDetails extends React.Component {
                                     fullWidth
                                     label="Nick Name"
                                     name="user_name"
-                                    onChange={this.handleChange}
+                                    onChange={(e) => this.handleChange(e)}
                                     required
                                     value={this.state.isReadonly ? this.state.user.user_name : this.state.new_user.user_name}
                                     variant="outlined"
@@ -137,7 +175,7 @@ export default class AccountProfileDetails extends React.Component {
                                     fullWidth
                                     label="Email"
                                     name="user_email"
-                                    onChange={this.handleChange}
+                                    onChange={(e) => this.handleChange(e)}
                                     InputProps={{
                                         readOnly: true,
                                         startAdornment: (
@@ -156,32 +194,17 @@ export default class AccountProfileDetails extends React.Component {
                                 md={6}
                                 xs={12}
                             >
-                                {/*<TextField*/}
-                                {/*    fullWidth*/}
-                                {/*    label="Birthday"*/}
-                                {/*    helperText="Please input your birthday!"*/}
-                                {/*    name="user_date_of_birth"*/}
-                                {/*    onChange={this.handleChange}*/}
-                                {/*    required*/}
-                                {/*    InputProps={{*/}
-                                {/*        readOnly: this.state.isReadonly,*/}
-                                {/*        startAdornment: (*/}
-                                {/*            <InputAdornment position="start">*/}
-                                {/*                <AccountCircle/>*/}
-                                {/*            </InputAdornment>*/}
-                                {/*        ),*/}
-                                {/*    }}*/}
-                                {/*    value={this.state.isReadonly ? this.state.user.user_date_of_birth : this.state.user.user_date_of_birth}*/}
-                                {/*    variant="outlined"*/}
-                                {/*/>*/}
                                 <LocalizationProvider dateAdapter={AdapterDateFns}>
                                     <DatePicker
+                                        fullWidth
+                                        disabled={this.state.isReadonly}
                                         label="Birthday"
-                                        value={this.state.user.user_date_of_birth}
-                                        onChange={(newValue) => {
-                                            this.handleChange()
-                                        }}
-                                        renderInput={(params) => <TextField {...params} />}
+                                        value={this.state.isReadonly ? this.state.user.user_date_of_birth :this.state.new_user.user_date_of_birth}
+                                        onChange={(newVal) =>
+                                            this.handleChange(newVal)
+                                        }
+                                        renderInput={(params) => <TextField {...params}
+                                                                            helperText={params ? "" : "Please input your Birthday"}/>}
                                     />
                                 </LocalizationProvider>
                             </Grid>
@@ -194,7 +217,7 @@ export default class AccountProfileDetails extends React.Component {
                                     fullWidth
                                     label="Phone Number"
                                     name="user_mobile"
-                                    onChange={this.handleChange}
+                                    onChange={(e) => this.handleChange(e)}
                                     InputProps={{
                                         readOnly: this.state.isReadonly,
                                         startAdornment: (
@@ -219,7 +242,7 @@ export default class AccountProfileDetails extends React.Component {
                                     fullWidth
                                     label="Country"
                                     name="user_country"
-                                    onChange={this.handleChange}
+                                    onChange={(e) => this.handleChange(e)}
                                     select
                                     SelectProps={{native: true}}
                                     required
@@ -232,8 +255,10 @@ export default class AccountProfileDetails extends React.Component {
                                 >
                                     {addrArea.map((option) => (
                                         <option
+
                                             key={option.country.value}
                                             value={option.country.value}
+
                                         >
                                             {option.country.label}
                                         </option>
@@ -250,7 +275,7 @@ export default class AccountProfileDetails extends React.Component {
                                     fullWidth
                                     label="State"
                                     name="user_state"
-                                    onChange={this.handleChange}
+                                    onChange={(e) => this.handleChange(e)}
                                     disabled={this.state.isReadonly}
                                     required
                                     select
@@ -262,6 +287,7 @@ export default class AccountProfileDetails extends React.Component {
                                         <option
                                             key={option.value}
                                             value={option.value}
+                                            onClick={(e) => this.handleChange(e)}
                                         >
                                             {option.label}
                                         </option>
@@ -277,7 +303,7 @@ export default class AccountProfileDetails extends React.Component {
                                     fullWidth
                                     label="Street"
                                     name="user_detail_street"
-                                    onChange={this.handleChange}
+                                    onChange={(e) => this.handleChange(e)}
                                     InputProps={{
                                         readOnly: this.state.isReadonly,
                                         startAdornment: (
@@ -305,7 +331,7 @@ export default class AccountProfileDetails extends React.Component {
                                 <Button
                                     color="primary"
                                     variant="contained"
-                                    onClick={() => this.HandleClick()}
+                                    onClick={() => this.Submit()}
                                 >
                                     {this.state.isReadonly ? "Change Profiles" : "Save details"}
                                 </Button>
@@ -326,29 +352,35 @@ export default class AccountProfileDetails extends React.Component {
     }
 
 
-    HandleClick() {
-        if (!this.state.isReadonly) {
-            console.log(this.state.user)
-        } else {
-            this.setState({
-                new_user: {
-                    "id": "",
-                    "user_name": "",
-                    "user_email": "",
-                    "user_date_of_birth": "",
-                    "user_mobile": "",
-                    "user_country": "",
-                    "user_state": "",
-                    "user_detail_street": "",
-                    "user_address": ""
+    Submit() {
+        if (!this.state.isReadonly) { //submit
+            // console.log(this.state.new_user)
+            const data = this.state.new_user
+            data["user_address"] = [data["user_country"], data["user_state"], data["user_detail_street"]].join(",")
+            // this.setState({new_user: this.state.user})
+            const that = this
+            // console.log(sessionStorage.getItem("id"))
+            axios.put("http://localhost:5000/user_information/user_profile/update_user_information", {
+
+                    "id": sessionStorage.getItem("id"),
+                    "user_name": data.user_name,
+                    "user_mobile": data.user_mobile,
+                    "user_date_of_birth": data.user_date_of_birth,
+                    "user_address": data.user_address
                 }
-            })
+            ).then(
+                that.setState({user: data, isReadonly: true}) //after axios
+                // console.log("success")
+            )
+
+        } else {
+            this.setState({isReadonly: false})
         }
-        this.setState({isReadonly: !this.state.isReadonly})
+
     }
 
+
     ListState(user_country) {
-        console.log(user_country)
         if (user_country === "" || user_country === undefined) {
             user_country = "Australia"
         }
