fa8a2559453a8b70ac33f6a4a6c52d6e142f4614
wtxqooi <2910842215@qq.com>
2022-05-08 12:07:46 +0800

    router guard

--- frontend/Testing/Unit_Test/Unit_Test_Login.py
+++ frontend/Testing/Unit_Test/Unit_Test_Login.py
@@ -26,7 +26,7 @@ class TestLogin(unittest.TestCase):
 
     # driver.find_element_by_partial_link_text("CSDN").click()
     # sleep(5)
-    def bad_password(self):
+    def good_username_bad_password(self):
         """Login with correct username and bad password"""
         driver = self.driver
         driver.find_element_by_xpath('//*[@id="email"]').clear()
@@ -40,5 +40,80 @@ class TestLogin(unittest.TestCase):
         self.assertEqual(dig_alert.text, "Please input correct password")  # 预期测试结果
         sleep(1)
 
+    def incorrect_username_good_password(self):
+        """Login with incorrect username and correct password"""
+        driver = self.driver
+        driver.find_element_by_xpath('//*[@id="email"]').clear()
+        driver.find_element_by_xpath('//*[@id="email"]').send_keys("2910842225@qq.com")
+        driver.find_element_by_xpath('//*[@id="password"]').clear()
+        driver.find_element_by_xpath('//*[@id="password"]').send_keys("12345678")
+        driver.find_element_by_xpath('//*[@id="root"]/main/div/form/button').click()
+        sleep(5)
+        dig_alert = driver.switch_to.alert
+
+        self.assertEqual(dig_alert.text, "User did not exit, please sign up first")  # 预期测试结果
+        sleep(1)
+
+    def bad_username_good_password(self):
+        """Login with bad username and correct password"""
+        driver = self.driver
+        driver.find_element_by_xpath('//*[@id="email"]').clear()
+        driver.find_element_by_xpath('//*[@id="email"]').send_keys("2910842225")
+        driver.find_element_by_xpath('//*[@id="password"]').clear()
+        driver.find_element_by_xpath('//*[@id="password"]').send_keys("12345678")
+        driver.find_element_by_xpath('//*[@id="root"]/main/div/form/button').click()
+        sleep(5)
+        dig_alert = driver.switch_to.alert
+
+        self.assertEqual(dig_alert.text, "please input valid Email")  # 预期测试结果
+        sleep(1)
+
+    def empty_username_empty_password(self):
+        """Login with empty username and empty password"""
+        driver = self.driver
+        driver.find_element_by_xpath('//*[@id="email"]').clear()
+        driver.find_element_by_xpath('//*[@id="email"]').send_keys("")
+        driver.find_element_by_xpath('//*[@id="password"]').clear()
+        driver.find_element_by_xpath('//*[@id="password"]').send_keys("")
+        driver.find_element_by_xpath('//*[@id="root"]/main/div/form/button').click()
+        sleep(5)
+        dig_alert = driver.switch_to.alert
+
+        self.assertEqual(dig_alert.text, "please input password")  # 预期测试结果
+        sleep(1)
+
+    def bad_username_empty_password(self):
+        """Login with empty username and empty password"""
+        driver = self.driver
+        driver.find_element_by_xpath('//*[@id="email"]').clear()
+        driver.find_element_by_xpath('//*[@id="email"]').send_keys("1")
+        driver.find_element_by_xpath('//*[@id="password"]').clear()
+        driver.find_element_by_xpath('//*[@id="password"]').send_keys("")
+        driver.find_element_by_xpath('//*[@id="root"]/main/div/form/button').click()
+        sleep(5)
+        dig_alert = driver.switch_to.alert
+
+        self.assertEqual(dig_alert.text, "please input password")  # 预期测试结果
+        sleep(1)
+
+    def empty_username_bad_password(self):
+        """Login with empty username and empty password"""
+        driver = self.driver
+        driver.find_element_by_xpath('//*[@id="email"]').clear()
+        driver.find_element_by_xpath('//*[@id="email"]').send_keys("")
+        driver.find_element_by_xpath('//*[@id="password"]').clear()
+        driver.find_element_by_xpath('//*[@id="password"]').send_keys("1")
+        driver.find_element_by_xpath('//*[@id="root"]/main/div/form/button').click()
+        sleep(5)
+        dig_alert = driver.switch_to.alert
+
+        self.assertEqual(dig_alert.text, "please input valid Email")  # 预期测试结果
+        sleep(1)
+
     def tearDown(self):
         self.driver.quit()
+
+
+if __name__ == '__main__':
+    # print()
+    print(list(set(dir(TestLogin)) ^ set(dir(unittest.TestCase))))
--- frontend/Testing/run_test.py
+++ frontend/Testing/run_test.py
@@ -6,8 +6,10 @@ test_dir = './Unit_Test'
 
 if __name__ == '__main__':
     testunit = unittest.TestSuite()  # 生成单元测试流程
-    testunit.addTest(TestLogin("normal"))
-    testunit.addTest(TestLogin("bad_password"))  # 加载测试用例
+    loginTestList = list(set(dir(TestLogin)) ^ set(dir(unittest.TestCase)))
+    for test in loginTestList:
+        testunit.addTest(TestLogin(test))
+    # testunit.addTest(TestLogin("bad_password"))  # 加载测试用例
     fp = open('./unittest.html', 'wb')  # 创建测试报告，以写的方式存入某个路径
     runner = HTMLTestRunner(
         stream=fp,
