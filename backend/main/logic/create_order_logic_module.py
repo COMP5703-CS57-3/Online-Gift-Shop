44f58e931c648ebcc9365a535ab0b516f44d4fa4
ZhengLi74 <101176546+ZhengLi74@users.noreply.github.com>
2022-05-21 11:24:25 +0800

    Add files via upload

--- backend/main/logic/create_order_logic_module.py
+++ backend/main/logic/create_order_logic_module.py
@@ -4,7 +4,7 @@ import json
 
 from ..service.create_an_pay import create_checkout_session, pay_order_success, pay_order_fail
 from ..util.dto import create_order_part_dto
-from ..service.create_order import process_order_create
+# from ..service.create_order import process_order_create
 from ..service.delete_order import process_delete_order
 from ..service.search_an_order import search_an_order_method
 from ..service.pay_an_order import pay_an_order_method, set_an_order_as_delivery, set_an_order_as_completed
@@ -12,21 +12,21 @@ create_order_part_namespace = create_order_part_dto.create_order_part_namespace
 from flask import request, redirect
 
 
-@create_order_part_namespace.route('/create')
-class CreateOrder(Resource):
-    @staticmethod
-    @create_order_part_namespace.expect(create_order_part_dto.create_order_input_format)
-    @create_order_part_namespace.response(200, 'success', model=create_order_part_dto.create_order_output_format)
-    @create_order_part_namespace.response(404, 'not found')
-    @create_order_part_namespace.response(403, 'Bad request')
-    def post():
-        resp = process_order_create(json.loads(request.data))
-        if resp.status_code == 404:
-            return marshal(resp, create_order_part_dto.order_output_format), 404
-        elif resp.status_code == 403:
-            return marshal(resp.response_data, create_order_part_dto.order_gift_stock_output_format), 403
-        else:
-            return marshal(resp.response_data, create_order_part_dto.create_order_output_format), 200
+# @create_order_part_namespace.route('/create')
+# class CreateOrder(Resource):
+#     @staticmethod
+#     @create_order_part_namespace.expect(create_order_part_dto.create_order_input_format)
+#     @create_order_part_namespace.response(200, 'success', model=create_order_part_dto.create_order_output_format)
+#     @create_order_part_namespace.response(404, 'not found')
+#     @create_order_part_namespace.response(403, 'Bad request')
+#     def post():
+#         resp = process_order_create(json.loads(request.data))
+#         if resp.status_code == 404:
+#             return marshal(resp, create_order_part_dto.order_output_format), 404
+#         elif resp.status_code == 403:
+#             return marshal(resp.response_data, create_order_part_dto.order_gift_stock_output_format), 403
+#         else:
+#             return marshal(resp.response_data, create_order_part_dto.create_order_output_format), 200
 
 @create_order_part_namespace.route('/delete/<user_id>/<order_number>')
 class DeleteOrder(Resource):
@@ -82,7 +82,7 @@ class PayResultCallback(Resource):
         if args.get('success'):
             resp = pay_order_success(order_id)
             if resp.status_code == 200:
-                return redirect("http://localhost:3000/", code=303)
+                return redirect("http://34.203.190.170/", code=303)
         else:
             resp = pay_order_fail(order_id)
             if resp.status_code == 200:
